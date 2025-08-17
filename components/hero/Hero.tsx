637cb1d6c13116ffbf0f0ae84a4b53c3c449036e
Michael <89726435+Michael-YZhou@users.noreply.github.com>
2025-08-17 18:57:36 +1000

    Add a grid background to the Hero component.

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Spotlight } from "../ui/Spotlight";
+import { cn } from "@/lib/utils";
 
 const Hero = () => {
   return (
@@ -14,6 +15,19 @@ const Hero = () => {
           fill="purple"
         />
         <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+
+      <div className="relative flex h-screen w-full items-center justify-center bg-white bg-grid-black/[0.2] dark:bg-black-100 derk:bg-grid-white/[0.3]">
+        <div
+          className={cn(
+            "absolute inset-0",
+            "[background-size:40px_40px]",
+            "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
+            "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]"
+          )}
+        />
+        {/* Radial gradient for the container to give a faded look */}
+        <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100"></div>
       </div>
     </div>
   );
