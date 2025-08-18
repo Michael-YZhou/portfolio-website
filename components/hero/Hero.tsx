f10ac120f16ca9848b3afa22596d3b4f79d751b5
Michael <89726435+Michael-YZhou@users.noreply.github.com>
2025-08-18 19:57:14 +1000

    Adjust the visibility of the grid background in the Hero component.

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -17,17 +17,25 @@ const Hero = () => {
         <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
       </div>
 
-      <div className="relative flex h-screen w-full items-center justify-center bg-white bg-grid-black/[20] dark:bg-black-100 dark:bg-grid-white/[30]">
+      <div className="relative flex h-screen w-full items-center justify-center bg-white bg-grid-black/[0.02] dark:bg-black-100 dark:bg-grid-white/[0.03]">
         <div
           className={cn(
             "absolute inset-0",
             "[background-size:40px_40px]",
             "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
-            "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]"
+            "dark:[background-image:linear-gradient(to_right,#262626_0.03px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]"
           )}
         />
         {/* Radial gradient for the container to give a faded look */}
         <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100"></div>
+
+        <div className="flex justify-center relative my-20 z-10">
+          <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+            <h2 className="uppercase tracking-widest text-xs test-center text-blue-100 max-w-80">
+              Welcome to my portfolio website!
+            </h2>
+          </div>
+        </div>
       </div>
     </div>
   );
